b45159ffb35b8153ff220b0a0982d0c8f7ff797a
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-09-15 14:26:51 +0400

    api signin create

--- back-end/src/controller/auth.js
+++ back-end/src/controller/auth.js
@@ -1,5 +1,5 @@
 const User=require('../models/user');
-
+const jwt = require('jsonwebtoken');
 exports.signup =(req,res)=>{
     
     User.findOne({email:req.body.email})
@@ -37,4 +37,35 @@ exports.signup =(req,res)=>{
     });
 });
 
+}
+
+
+exports.signin = (req,res)=>{
+    User.findOne({email:req.body.email})
+    .exec((error, user)=>{
+        if (error) return res.status(400).json({error});
+
+        if (user) {
+           
+            if (user.authenticate(req.body.password)) {
+                
+                const token=jwt.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:'1h'});
+                const {firstName,lastName,email,role,fullName}=user;
+                if(res.status(200)){console.log("test");}
+                res.status(200).json({
+                    token,
+                    user:{
+                        firstName,lastName,email,role,fullName
+                    }
+                });
+            }else{
+                res.status(400).json({
+                    message:'Invalid Password'
+                })
+            }
+
+        } else {
+            return res.status(400).json({message:'Something went wrong'});
+        }
+    });
 }
