42c092e5c5f0db6a4c07f34939ccd3211628c9e8
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-09-17 19:13:43 +0400

    path package for image path and multer user for file download

--- back-end/src/routes/product.js
+++ back-end/src/routes/product.js
@@ -1,16 +1,23 @@
 const express=require('express');
-const router=express.Router();
 //const {addCategory,getCategories}=require('../controller/category');
 const {requireSignin,adminMiddleware}=require('../common-middleware');
+const {createProduct} =require('../controller/product');
+const multer=require('multer');
+const router=express.Router();
+const path=require('path');
+const shortid = require('shortid');
 
-const Product =require('../models/product');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname),'uploads'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, shortid.generate() + '-' + file.originalname)
+    }
+  })
 
-router.post('/product/create',requireSignin,adminMiddleware,(req,res)=>{
-
-    res.status(200).json({message:'Hello'});
-
-
-} );
+const upload=multer({storage});
+router.post('/product/create',requireSignin,upload.single('productPicture'),createProduct );
 //router.get('/category/getcategory',getCategories );
 
 module.exports =router;
