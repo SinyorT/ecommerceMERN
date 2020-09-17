eb727a7566c7bf8b2d5e6519bff58629dac302bd
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-09-17 17:20:51 +0400

    product model create

---
+++ back-end/src/models/product.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose');
+const productSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    slug:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    price:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    offer:{type:Number},
+    productPictures:[
+        {img:{type:String}}
+    ],
+    reviews:[
+        {
+            userId:mongoose.Schema.Types.ObjectId, ref:'User'
+        }
+    ],
+    category:{type:mongoose.Schema.Types.ObjectId, ref:'Category'},
+    createdBy:{ type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    updateAt:Date,
+},{timestamps:true});
+
+module.exports = mongoose.model('Product',productSchema);
