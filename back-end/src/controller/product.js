36cd04b8e3234b6496ee7229436e8e95552d035d
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-09-17 19:25:02 +0400

    multiple file donwload

--- back-end/src/controller/product.js
+++ back-end/src/controller/product.js
@@ -3,6 +3,6 @@ const shortid = require('shortid');
 
 
 exports.createProduct = (req, res) => {
-    return res.status(200).json({ file:req.file,body:req.body});
+    return res.status(200).json({ file:req.files,body:req.body});
 
 }
