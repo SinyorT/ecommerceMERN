c986a1f082e790f119e708315121b2726771846d
LAPTOP-0VJIAJQ5\99450 <abdurahmanov.togrul@gmail.com>
2020-09-17 11:53:57 +0400

    category router create

--- back-end/src/index.server.js
+++ back-end/src/index.server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 //routes
 const authRoutes=require('./routes/auth');
 const adminRoutes=require('./routes/admin/auth');
-
+const categoryRoutes=require('./routes/category');
 
 
 //environment variable or you can say constants
@@ -28,6 +28,7 @@ mongoose.connect(
 app.use(express.json());
 app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
+app.use('/api',categoryRoutes);
 
 app.get('/',(req,res,next)=>{
     res.status(200).json({
